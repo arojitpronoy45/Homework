0ccd4d5dac03b60397582c18973403a367f8a95f
arojitpronoy45 <arojitpronoyqa@gmail.com>
2023-08-27 22:29:10 -0700

    homework

---
+++ src/hw04_02JavaVariablesInitialized/AboutMeTest.java
@@ -0,0 +1,10 @@
+package hw04_02JavaVariablesInitialized;
+
+
+public class AboutMeTest {
+	public static void main(String[] args) {
+
+		AboutMe arojit = new AboutMe(); // constructor initialized here
+		arojit.aboutMe(); // method initialized here
+	}
+}
